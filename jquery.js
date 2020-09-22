c59f25682b11e5f1dc7a5b0def02ecbb0b6c43ce
Atif Haque <61943868+atifih@users.noreply.github.com>
2020-09-22 14:25:25 +1000

    Front end just about complete

--- jquery.js
+++ jquery.js
@@ -1,5 +1,12 @@
-$(document).ready(function(){
-    $(".saveBtn").on("click", function() {
-        alert("I've been clicked!");
+
+$(document).ready(function () {
+    $(".saveBtn").on("click", function (e) {
+        e.preventDefault();
+        var hour = $(this).attr("data-timeslot")
+        var details = document.querySelector("#text" + hour).value;
+        console.log(details);
+        console.log(hour);
+        var appointment = localStorage.setItem(hour, details);
+        console.log(appointment);
     });
 });
