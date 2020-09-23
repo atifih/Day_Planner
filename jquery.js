386292dff3234fbe2c52c70477b03e4a4dfbc2e3
Atif Haque <61943868+atifih@users.noreply.github.com>
2020-09-23 10:41:04 +1000

    Attempting to implement local storage

--- jquery.js
+++ jquery.js
@@ -1,12 +1,67 @@
+var hour;
+var Details;
+var appDetails = [{ hour, Details }];
 
-$(document).ready(function () {
-    $(".saveBtn").on("click", function (e) {
-        e.preventDefault();
-        var hour = $(this).attr("data-timeslot")
-        var details = document.querySelector("#text" + hour).value;
-        console.log(details);
-        console.log(hour);
-        var appointment = localStorage.setItem(hour, details);
-        console.log(appointment);
+function renderAppDetails() {
+
+    //loop from 9 till 12
+    for (var i = 9; i <= 12; i++) {
+        var str = i.toString();
+        var tex;
+
+        tex = JSON.parse(localStorage.getItem(str));
+        $(".container").append(tex);
+        //loop from 1 till 5
+        for (var i = 1; i <= 5; i++) {
+            var str = i.toString();
+            var tex;
+
+            tex = JSON.parse(localStorage.getItem(str));
+            $(".container").append(tex);
+            //get element by id for the respective hour
+            //set the text of the element you retrieved to tex
+        }
+
+
+
+
+
+    }
+}
+
+
+
+
+/*
+function init(){
+    storedappDetails = JSON.parse(localStorage.getItem("Details"));
+ 
+}
+ 
+*/
+
+function storeAppDetails() {
+    $(document).ready(function () {
+        $(".saveBtn").on("click", function (e) {
+            e.preventDefault();
+            var hour = $(this).attr("data-timeslot")
+            var details = document.querySelector("#text" + hour).value;
+            console.log(details);
+            console.log(hour);
+            appDetails.push(details);
+            localStorage.setItem(hour, JSON.stringify(details));
+            var storedAppDetails = JSON.parse(localStorage.getItem("appDetails"));
+            if (storedAppDetails !== null) {
+                appDetails = storedAppDetails;
+            }
+
+
+
+        });
     });
-});
+
+
+}
+renderAppDetails();
+storeAppDetails();
+
